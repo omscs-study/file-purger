d708071c9f30d8ca5a63e10cec70833a6e6d070b
Allan Reyes <allanbreyes@gmail.com>
2017-07-30 21:47:32 -0700

    Add Slack messaging

--- purge.rb
+++ purge.rb
@@ -33,14 +33,36 @@ def delete_files(file_ids)
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
     log.info "#{file_id}: #{JSON.parse(response.body)['ok']}"
+    file_id
   end
 end
 
-if __FILE__ == $0
-  raise "No SLACK_TOKEN specified" unless @token
+def post_message(message, options = {})
+  params = {
+    token: @token,
+    text: message,
+    channel: '#admins',
+    as_user: false,
+    username: 'file-purger'
+  }.merge(options)
+  uri = URI.parse('https://slack.com/api/chat.postMessage')
+  uri.query = URI.encode_www_form(params)
+  Net::HTTP.get_response(uri)
+end
+
+def call
+  raise 'No SLACK_TOKEN specified' unless @token
   log.info 'Deleting files...'
-  files = list_files
-  file_ids = files.map { |f| f['id'] }
-  delete_files(file_ids)
+  post_message 'Starting file purge...'
+  file_ids = list_files.map { |f| f['id'] }
+  deleted_file_ids = delete_files(file_ids)
   log.info 'Done!'
+  post_message "#{deleted_file_ids.length} files purged!", icon_emoji: ':smile:'
+rescue => exception
+  raise exception
+  post_message "File purger failed: #{exception.message}", icon_emoji: ':scream:'
+end
+
+if __FILE__ == $0
+  call
 end
